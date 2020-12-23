c9fb8a8d219bd40ec4aebce4cd9bfbd478e6fd17
gepran <gepran@gmail.com>
2020-12-23 17:23:18 +0400

    commit 5

--- assets/js/main-script.js
+++ assets/js/main-script.js
@@ -1,4 +1,6 @@
 
+formValidation();
+
 function mapPowRegData() {
     let alcohol_percentages = document.getElementsByClassName('alcohol-percentage');
     let raw_readings = document.getElementsByClassName('raw-reading');
@@ -18,12 +20,35 @@ function calculateAlcoholPercentage(){
     let calculation_raw = document.getElementById('calculation-raw').value;
     let calculation_result = document.getElementById('calculation-result');
     let data = mapPowRegData();
-    let powReg = regression('power', data);
-    let coefficient = parseFloat(powReg.equation[0]);
-    let exponent = parseFloat(powReg.equation[1]);
-    let alcohol_percentage = coefficient * (parseFloat(calculation_raw) ^ exponent);
 
-    calculation_result.innerHTML = alcohol_percentage.toFixed(2);
-
-    console.log(alcohol_percentage);
+    let powReg = regression.power(data, {precision: 20}).predict(calculation_raw);
+    calculation_result.innerHTML = powReg[1].toFixed(2);
+    console.log(powReg[1]);
 }
+
+function formValidation() {
+    var forms = document.querySelectorAll('.needs-validation');
+    let calculation_btn = document.getElementById('calculation-btn');
+
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            calculation_btn.addEventListener('click', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+
+   /* calculation_btn.addEventListener('click', function (event) {
+        if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+    }, false)*/
+}
+
