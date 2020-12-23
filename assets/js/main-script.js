d210d5515f96d233cbc00093f4e26bcfbe1872c6
gepran <gepran@gmail.com>
2020-12-23 14:54:10 +0400

    commit 3

--- assets/js/main-script.js
+++ assets/js/main-script.js
@@ -1,7 +1,29 @@
-(function() {
-    "use strict";
 
-    var vm = this;
+function mapPowRegData() {
+    let alcohol_percentages = document.getElementsByClassName('alcohol-percentage');
+    let raw_readings = document.getElementsByClassName('raw-reading');
+    let dataToMap = [];
 
+    for (let i = 0, len = alcohol_percentages.length; i < len; i++) {
+        dataToMap.push([
+            parseFloat(alcohol_percentages[i].value),
+            parseInt(raw_readings[i].value || 0),
+        ])
+    }
 
-})();
+    return dataToMap
+}
+
+function calculateAlcoholPercentage(){
+    let calculation_raw = document.getElementById('calculation-raw').value;
+    let calculation_result = document.getElementById('calculation-result');
+    let data = mapPowRegData();
+    let powReg = regression('power', data);
+    let coefficient = parseFloat(powReg.equation[0]);
+    let exponent = parseFloat(powReg.equation[1]);
+    let alcohol_percentage = coefficient * (parseFloat(calculation_raw) ^ exponent);
+
+    calculation_result.innerHTML = alcohol_percentage;
+
+    console.log(alcohol_percentage);
+}
